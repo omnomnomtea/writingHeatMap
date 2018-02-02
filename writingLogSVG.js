7ab2d9e7f5e68721f46748fb0c975d7502ba5ce7
Tess Monical <tess.monical@gmail.com>
2018-02-01 20:26:10 -0600

    generating a sub-SVG

--- writingLogSVG.js
+++ writingLogSVG.js
@@ -1,12 +1,16 @@
 const writingLog = require('./writinglog.json');
 
 const colors = [
+  '#FFFFFF',
   '#E9DCED',
   '#D0C4D3',
   '#B7ADBA',
   '#9E95A0',
   '#847D87',
 ];
+// size of each box in px
+const boxSize = 10;
+
 
 const mapDataToWordsPerDay = (data) => {
   const wpd = [];
@@ -21,15 +25,64 @@ const mapDataToWordsPerDay = (data) => {
   return wpd;
 };
 
-const generateSVG = (data) => {
+const generateSVG = (rawData) => {
   // clean the data and put in reverse chronological order
-  const cleanerData = mapDataToWordsPerDay(data);
-  const sortedData = cleanerData.sort((a, b) => b.date - a.date);
+  rawData.sort((a, b) => b.date - a.date);
 
+  // combine entries to max of one per day
+  const data = mapDataToWordsPerDay(rawData);
+
+  const recentDate = data[data.length - 1].date;
+  const olderDate = data[0].date;
+
+  const msPerDay = 1000 * 60 * 60 * 24;
+  const maxCount = Math.max(...data.map(d => d.count));
+
+
+  const generateRect = (dayData, offsetX, offsetY) => {
+    let colorIndex = 0; // color defaults to white or index 0
+    if (dayData.count) colorIndex =
+      Math.floor((dayData.count) / (maxCount + 1) * (colors.length - 1)) + 1;
+    const color = colors[colorIndex];
+    return `<rect class="day" width="${boxSize}" height="${boxSize}" x="${offsetX}" y="${offsetY}" fill="${color}" data-count="${dayData.count}" data-date="${dayData.date.toDateString()}"></rect>`;
+  };
+
+  let dataIndex = 0;
+  const dataToMap = [];
+  const startSunday = olderDate - (olderDate.getDay() * msPerDay);
+  const endsSaturday = new Date(recentDate + ((6 - recentDate.getDay()) * msPerDay));
+  // console.log('startSunday', startSunday);
+  for (
+    let currentDay = new Date(startSunday);
+    currentDay < endsSaturday;
+    currentDay = new Date(currentDay.valueOf() + msPerDay)
+  ) {
+    // if we're within a day of the data's timestamp, use data's wordcount
+    if (Math.abs(currentDay - data[dataIndex].date) < msPerDay) {
+      dataToMap.push(data[dataIndex]);
+      dataIndex += 1;
+    } else {
+      dataToMap.push({ date: new Date(currentDay), count: 0 });
+    }
+  }
+
+  // console.log(dataToMap);
+
+  const weeks = [];
+  dataToMap.forEach((datum) => {
+    const dayOfWeek = new Date(datum.date).getDay();
+    const rect = generateRect(datum, 12 * weeks.length, 12 * dayOfWeek);
+    // if it's 0, it's sunday and we start a new week
+    if (!dayOfWeek) {
+      weeks.push([rect]);
+    } else {
+      weeks[weeks.length - 1].push(rect);
+    }
+  });
+
+
+  return weeks.map(week => week.join('\n')).join('\n\n');
 };
 
-// const generateRect = (dayData) => {
-//   return `<rect class="day" width="10" height="10" x="-2" y="0" fill="#ebedf0" data-count="0" data-date="2017-05-14"></rect>`
-// };
 
-generateSVG(writingLog);
+console.log(generateSVG(writingLog));
