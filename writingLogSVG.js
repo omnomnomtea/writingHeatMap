d1f80ce7ca24691748ddfe9bffce5152769f120d
Tess Monical <tess.monical@gmail.com>
2018-02-08 09:20:14 -0600

    fixes type issue

--- writingLogSVG.js
+++ writingLogSVG.js
@@ -111,6 +111,10 @@ d3.csv('https://raw.githubusercontent.com/omnomnomtea/writingHeatMap/d3/log.csv'
       }
       return acc;
     }, [])
+    .map((d) => {
+      d.value = Number(d.value)
+      return d;
+    })
     .reduce((acc, obj) => {
       acc[format(obj.key)] = obj.value;
       return acc;
@@ -124,128 +128,3 @@ d3.csv('https://raw.githubusercontent.com/omnomnomtea/writingHeatMap/d3/log.csv'
     .text(d => d + ': ' + (data[d]));
 
 });
-
-
-// // size of each box in px
-// const boxSize = 20;
-// let maxCount;
-
-// const groupByDay = (data) => {
-//   const wpd = data.reduce((acc, entry) => {
-//     const timeSinceLast = acc.length ? acc[acc.length - 1].date - entry.date : Infinity;
-//     if (timeSinceLast < msPerDay / 2) {
-//       acc[acc.length - 1].count += entry.count;
-//     } else {
-//       acc.push(entry);
-//     }
-//     return acc;
-//   }, []);
-
-//   return wpd;
-// };
-
-// const generateSVG = (rawData) => {
-  // clean the data and put in reverse chronological order
-  // const lessRawData = rawData.map(d => ({ ...d, date: new Date(new Date(d.date).setHours(0, 0, 1)) }));
-//   lessRawData.sort((a, b) => b.date.valueOf() - a.date.valueOf());
-//   // combine entries to max of one per day
-//   const data = groupByDay(lessRawData);
-
-//   console.log('sorted and tidied data', data);
-
-//   const today = new Date(new Date().setHours(0, 0, 1));
-//   const olderDate = data[data.length - 1].date;
-
-//   maxCount = Math.floor(Math.max(...data.map(d => d.count)));
-
-//   const generateRect = (dayData) => {
-//     const offsetX = dayData.date.getDay() * (boxSize + 2);
-//     let colorIndex = 0; // color defaults to white (index 0)
-//     if (dayData.count) {
-//       colorIndex =
-//         Math.floor((dayData.count) / (maxCount + 1) * (colors.length - 1)) + 1;
-//     }
-//     const endOfLastWeek = new Date(today.valueOf() + (6 - today.getDay()) * msPerDay);
-//     const daysAgo = Math.floor((endOfLastWeek.valueOf() - dayData.date.valueOf()) / msPerDay);
-//     const weeksAgo = Math.floor((daysAgo) / 7);
-//     const color = colors[colorIndex];
-//     const offsetY = weeksAgo * (boxSize + 2);
-//     return `<rect class="day" width="${boxSize}" height="${boxSize}" x="${offsetX}" y="${offsetY}" fill="${color}" data-count="${dayData.count}" data-date="${dayData.date.toDateString()}"></rect>`;
-//   };
-
-//   // let's go through every date since olderDate
-//   // if we have data, keep it
-//   // if we don't, the default count is 0
-//   let dataIndex = 0;
-//   const dataToMap = [];
-//   for (
-//     let currentDay = new Date(today + msPerDay / 2);
-//     currentDay >= olderDate;
-//     currentDay = new Date(currentDay.valueOf() - msPerDay)
-//   ) {
-//     // if we're within a day of the data's timestamp, use data's wordcount
-//     if (data[dataIndex] && Math.abs(currentDay - data[dataIndex].date) < msPerDay / 2) {
-//       dataToMap.push(data[dataIndex]);
-//       dataIndex += 1;
-//     } else {
-//       dataToMap.push({ date: new Date(currentDay), count: 0 });
-//     }
-//   }
-
-//   console.log('days', dataToMap);
-
-//   const endOfWeek = new Date((6 - today.getDay()) * msPerDay + today.valueOf());
-//   const totalWeeksAgo = Math.ceil((endOfWeek.valueOf() - olderDate.valueOf()) / 7 / msPerDay) - 1;
-//   const daysSVG = dataToMap.map(day => generateRect(day));
-//   const svgString = daysSVG.join('\n');
-
-//   return `<svg width="${24 * (boxSize + 2)}" height="${(boxSize + 2) * (totalWeeksAgo + 2)}">
-//   <rect width="${24 * (boxSize + 2)}" height="${(boxSize + 2) * (totalWeeksAgo + 3)}" fill="#ffffff"></rect>
-
-//   <g transform="translate(2,${(boxSize + 2)})">
-//   ${svgString}
-//   <text class="label-week" x="${(boxSize + 2.5) * 7}" y="${(boxSize - 6)}" fill="${text_color}">This week</text>
-//   <text class="label-week" x="${(boxSize + 2.5) * 7}" y="${(boxSize * 2 - 4)}" fill="${text_color}">Last week</text>
-//   <text class="label-week" x="${(boxSize + 2.5) * 7}" y="${(boxSize * 3 - 2)}" fill="${text_color}">etc...</text>
-
-//   <g class="keys" transform="translate(${(boxSize + 2.5) * 9},0)">
-//   <g class="color-keys">
-
-//       <rect class="key" width="${boxSize}" height="${boxSize}" x="${(boxSize + 2.5) * 9}" y="${(boxSize + 2) * 5 + 10}" fill="${colors[0]}"></rect>
-
-//       <rect class="key" width="${boxSize}" height="${boxSize}" x="0" y="${(boxSize + 2) * 6}" fill="${colors[1]}"></rect>
-
-//       <rect class="key" width="${boxSize}" height="${boxSize}" x="0" y="${(boxSize + 2) * 7}" fill="${colors[2]}"></rect>
-
-//       <rect class="key" width="${boxSize}" height="${boxSize}" x="0" y="${(boxSize + 2) * 8}" fill="${colors[3]}"></rect>
-
-//       <rect class="key" width="${boxSize}" height="${boxSize}" x="0" y="${(boxSize + 2) * 9}" fill="${colors[4]}"></rect>
-
-//       <rect class="key" width="${boxSize}" height="${boxSize}" x="0" y="${(boxSize + 2) * 10}" fill="${colors[5]}"></rect>
-
-//     </g>
-
-//     <text class="key-label" x="0" y="${0}" fill="${text_color}">0</text>
-//     <text class="key-label" x="0" y="${(boxSize + 28) * 5 + 10}" fill="${text_color}">>=${maxCount}</text>
-//   </g>
-//   </g>
-//   </svg>`;
-// };
-
-
-// const svg = generateSVG(writingLog);
-
-// const insertLoc = document.getElementById('insert-location');
-// insertLoc.innerHTML = svg;
-
-// let days = document.getElementsByClassName('day');
-// days = Array.prototype.slice.call(days);
-
-
-// days.forEach((day) => {
-//   const textNode = document.createElement('text');
-//   textNode.innerHTML = `${day.dataset.count} words on ${day.dataset.date}`;
-//   textNode.classList.add('tooltip');
-//   textNode.classList.add('tooltiptext');
-//   day.parentNode.insertBefore(textNode, day);
-// });
